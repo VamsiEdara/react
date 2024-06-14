bdf647ac0c7246e9688b0f350b61590661a9a185
Vamsi Edara 26 <96575839+VamsiEdara@users.noreply.github.com>
2024-06-14 14:18:10 +0000

    Added Componets folder

---
+++ components/components.js
@@ -0,0 +1,15 @@
+function App() {
+    return <h1>Component 🤝</h1>;
+  }
+  
+  export default App;
+
+
+// Class based Components
+
+//   import { Component } from 'react';
+//   class Greeting extends Component {
+//     render() {
+//       return <h1>Hello, {this.props.name}!</h1>;
+//     }
+//   }
